6b0e5435f58b2c105b72481b58b4944311b13623
Yosita satiman <yosita_satiman@cmu.ac.th>
2024-12-16 20:38:43 +0700

    lab9

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -2,10 +2,15 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
+    
+    double loan,rpy,cpy;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rpy;
 	cout << "Enter amount you can pay per year: ";
+	cin >> cpy;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +25,22 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
+	for(int n=1;loan>0;n++){
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
+	cout << setw(13) << left << n; 
+	cout << setw(13) << left << loan;
+	double interest = loan*(rpy/100);
+	cout << setw(13) << left << interest;
+	double total = loan+interest;
+	cout << setw(13) << left << total;
+	    if(cpy>total){
+	        cpy = total;
+	    }
+	cout << setw(13) << left << cpy;
+	cout << setw(13) << left << total-cpy;
 	cout << "\n";	
+	loan = total-cpy;
+	}
 	
 	return 0;
 }
--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,6 +2,26 @@
 using namespace std;
 
 //Write the function printO() here
+void printO(int N,int M)
+{
+    if(N<=0 || M<=0){
+        cout << "Invalid input";
+    }else if(N>0){
+        int i = 0;
+        while(i<N){
+            if(M>0){
+                int j=0;
+                while(j<M){
+                    cout << "O";
+                    j++;
+                }
+                cout <<"\n";
+            }
+            i++;
+        }
+    }
+    
+}
 
 int main(){
 	
